520f8622f2367a2522ddc7acf6eae1592b2d05ef
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-16 18:18:09 +0100

    feat: add city time and location

---
+++ src/components/City/TimeLocation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { formatToLocalTime } from '../../services/weather'
+
+const TimeLocation = ({weather}) => {
+
+  const { country, dt, name, timezone } = weather
+
+  return (
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-l font-extralight'>{formatToLocalTime(dt, timezone)}</p>
+        </div>
+
+        <div className='flex items-center justify-center my-3'>
+            <p className='text-white text-xl font-normal' >{name}, {country}</p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeLocation
