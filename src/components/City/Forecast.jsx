054eb167d28e8e555bcf5627c5049a7ba8a8a0bc
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-16 18:32:20 +0100

    feat: add daily and hourly forecasts for cities

---
+++ src/components/City/Forecast.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { getIconUrl } from '../../services/weather'
+
+const Forecast = ({ type, forecast }) => {
+
+  return (
+    <div>
+        <div className='flex items-center justify-start mt-6'>
+            <p className='text-white font-medium uppercase'>{type} forecast</p>
+        </div>
+            <hr className='my-2'/>
+
+            <div className='flex items-center justify-between text-white'>
+                {forecast.map((forecast, index) => (
+                    <div className='flex flex-col items-center justify-center' key={index}>
+                        <p className='font-light text-sm'>{forecast.time}</p>
+                        <img src={getIconUrl(forecast.icon)} alt="" className='w-12 my-1' />
+                        <p className='font-medium'>{forecast.temp.toFixed()}°</p>
+                    </div>
+                ))}
+            </div>
+    </div>
+  )
+}
+
+export default Forecast
