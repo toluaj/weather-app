2b19b41debd3cafc5beb936587c7738f657fb073
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-16 18:17:14 +0100

    feat: add city search

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,52 @@
+import  React, { useState } from 'react'
+import { UilSearch, UilMapMarker } from '@iconscout/react-unicons'
+import { toast } from 'react-toastify'
+
+const Inputs = ({ setQuery, setUnits, units }) => {
+
+  const [city, setCity] = useState('')
+
+  const handleCitySearch = () => {
+    if (city !== '') setQuery({ q: city })
+  }
+
+  const handleInputChange = (e) => {
+    setCity(e.target.value)
+  }
+
+  const handleUnitsChange = (e) => {
+    const selectedUnit = e.target.name
+    if (units !== selectedUnit) setUnits(selectedUnit)
+  }
+
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      toast.info('Fetching your location...')
+      navigator.geolocation.getCurrentPosition((position) => {
+        toast.dismiss()
+        let lat = position.coords.latitude
+        let lon = position.coords.longitude
+        setQuery({ lat, lon })
+      })
+    }
+  }
+
+  return (
+    <div className='flex justify-center my-6'>
+        <div className='flex w-3/4 items-center justify-start space-x-4'>
+          <input placeholder='Search city e.g. London' type="text" value={city} onChange={(e) => handleInputChange(e)} 
+            className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize placeholder:normal-case placeholder:text-lg' />
+            <UilSearch size={25} className='text-white cursor-pointer transition ease-out hover:scale-110' onClick={handleCitySearch}/>
+            <UilMapMarker size={25} className='text-white cursor-pointer transition ease-out hover:scale-110' onClick={handleLocationClick}/>
+        </div>
+
+        <div className='flex w-1/4 items-center justify-center'>
+            <button className='text-xl text-white font-light px-1' name='metric' onClick={handleUnitsChange}>°C</button>
+            <p className='text-xl text-white'>|</p>
+            <button className='text-xl text-white font-light px-1' name='imperial' onClick={handleUnitsChange}>°F</button>
+        </div>
+    </div>
+  )
+}
+
+export default Inputs
