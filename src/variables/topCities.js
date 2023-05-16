64da2e59e3f7c0a562174d35eae232b7e50bc7af
Tolu Ajia <toluajia@Tolus-MacBook-Pro-2.local>
2023-05-16 18:08:08 +0100

    chore: add top cities

---
+++ src/variables/topCities.js
@@ -0,0 +1,9 @@
+module.exports = {
+    cities: [
+        { id: 1, title: 'London' }, 
+        { id: 2, title: 'New York' }, 
+        { id: 3, title: 'Amsterdam' }, 
+        { id: 4, title: 'Lagos' }, 
+        { id: 5, title: 'Sydney' }, 
+    ]
+}
